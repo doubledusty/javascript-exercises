927f635cff2127f8d65bf69161fb0845eaa9c4c7
dustin <109438275+doubledusty@users.noreply.github.com>
2023-05-17 08:34:09 -0400

    first exercise finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, rep) {
+    if (rep < 0){
+        return "ERROR"
+    }
+    let outputString = ""
 
+    for(i = 0; i < rep; i++){
+    outputString = outputString.concat(inputString)
+
+    }
+    return outputString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    outputString = ""
 
+    for (i = 1; i < inputString.length; i++){
+        outputString = outputString.concat(inputString[inputString.length - i,inputString.length - i + 1])
+    }
+    return outputString
 };
 
 // Do not edit below this line
